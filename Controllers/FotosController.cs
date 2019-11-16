e5fb198a0ce6df3afd572b562b619659e3b6f391
Jesus_0414 <jesus140400@gmail.com>
2019-11-16 14:39:54 -0700

    Connnect database

--- Controllers/FotosController.cs
+++ Controllers/FotosController.cs
@@ -15,61 +15,62 @@ namespace ProyectoFinalHipermedia.Controllers
 
     public class FotosController: IFotos
     {
-        List<Fotos> fotosList=> new List<Fotos>
-        {
-            new Fotos
-            {
-             /*Names="1",
-             LastName="2",
-             Age=5,
-             Tittle="4",
-             Descript="5",
-             Email="5",
-             Social="3",*/
-            },
-        } ;
+      List<Fotos> fotosList=> new List<Fotos>
+      {
+          new Fotos
+          {
+            /*Names="1",
+            LastName="2",
+            Age=5,
+            Tittle="4",
+            Descript="5",
+            Email="5",
+            Social="3",*/
+          },
+      } ;
 
 
-        //ver como conectar  una base de datos libre 
-        string connectionString=@"data source=LAPTOP-ENL6O117\SQLEXPRESS; initial catalog=caliz; user id=2sim; password=1234";
-          
+      //ver como conectar  una base de datos libre 
+      string connectionString=@"data source=LAPTOP-UCMOS94G\SQLEXPRESS; initial catalog=db_portafolio; user id=Paula; password=12345";
+        
 
       [HttpGet("{id}")]
-        public Fotos GetFotos(int id)
+      public Fotos GetFotos(int id)
+      {
+          return fotosList[id];
+      }
+
+      [HttpGet]
+
+      public List<Fotos> GetFotosList ()
+      {
+        List<Fotos> fotoss=new List<Fotos>();
+        SqlConnection conn=new SqlConnection(connectionString);
+        SqlCommand cmd = new SqlCommand("select * from tb_portafolio", conn);////nombre de la tabla
+        conn.Open();
+        SqlDataReader reader= cmd.ExecuteReader();
+        while (reader.Read())
         {
-            return fotosList[id];
-        }
-
-        [HttpGet]
-
-        public List<Fotos> GetFotosList ()
-        {
-            List<Fotos> fotoss=new List<Fotos>();
-            SqlConnection conn=new SqlConnection(connectionString);
-            SqlCommand cmd = new SqlCommand("select*from tbl_photos", conn);////nombre de la tabla
-            conn.Open();
-            SqlDataReader reader= cmd.ExecuteReader();
-            while (reader.Read())
-            {
-             Fotos foto = new Fotos
-             {
-               Id= reader.GetInt64(reader.GetOrdinal("id")),
-               LastName =reader.GetString(reader.GetOrdinal("lastName")),
-               Names =reader.GetString(reader.GetOrdinal("names")),
-               Age =reader.GetInt32(reader.GetOrdinal("age")),///modificar la edad por le int 
-               Tittle =reader.GetString(reader.GetOrdinal("tittle")),
-               Descript =reader.GetString(reader.GetOrdinal("descript")),
-               Email =reader.GetString(reader.GetOrdinal("email")),
-               Social =reader.GetString(reader.GetOrdinal("social")),
-
-             };
-             fotoss.Add(foto);
-             ///
-             }
-           
-            conn.Close();
-            return fotoss;
-
+          Fotos foto = new Fotos
+          {
+            Id= reader.GetInt64(reader.GetOrdinal("id")),
+            LastName =reader.GetString(reader.GetOrdinal("lastName")),
+            Names =reader.GetString(reader.GetOrdinal("names")),
+            Age =reader.GetInt32(reader.GetOrdinal("age")),///modificar la edad por le int 
+            Tittle =reader.GetString(reader.GetOrdinal("tittle")),
+            Descript =reader.GetString(reader.GetOrdinal("descript")),
+            Email =reader.GetString(reader.GetOrdinal("email")),
+            Social =reader.GetString(reader.GetOrdinal("social")),
+            Imagen1 =reader.GetString(reader.GetOrdinal("imagen1")),               
+            Imagen2 =reader.GetString(reader.GetOrdinal("imagen2")),
+            Imagen3 =reader.GetString(reader.GetOrdinal("imagen3")),
+            Imagen4 =reader.GetString(reader.GetOrdinal("imagen4")),
+            Imagen5 =reader.GetString(reader.GetOrdinal("imagen5")),
+          };
+          fotoss.Add(foto);
+          }
+        conn.Close();
+        return fotoss;
       }
     }
 }
