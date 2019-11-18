627481f73ff876937f7219fed67724ad3dd2f87c
JenniD5 <elizabethduran0105@gmail.com>
2019-11-18 00:51:43 -0700

    intento1

---
+++ Frontend/backend.js
@@ -0,0 +1,21 @@
+const listaNombres = document.getElementById('listado-nombres');
+const columna = document.getElementById('columna1');  
+const ApiUrl = 'https://localhost:5001/hipermedia/fotos';
+
+let getData = async ()=> {
+    const respose = await fetch(`${ApiUrl}`);
+    const personasData = await respose.json();
+    console.log(personasData);
+    personasData.forEach(element => {
+        const {names, lastName, id} = element;
+        listaNombres.innerHTML  += `<li><a href="estructura.html?id=${id}" id = ${id}>${names} ${lastName}</a></li>`;
+        /*columna.innerHTML += `<div class="planetoide1">
+        <a href="estructura.html?id=${id}">
+            <img href="${imagen1}" width="25%"></a>
+        <div class="Texto">${names}</div>
+    </div>`;*/
+   
+   }); 
+}
+
+getData();
---
+++ Frontend/estructura.js
@@ -0,0 +1,42 @@
+const photoData = {
+    names : document.getElementById('data-Name'),
+    lastName : document.getElementById('data-LastName'),
+    age : document.getElementById('data-Age'),
+    tittle : document.getElementById('data-Tittle'),
+    descript : document.getElementById('data-Descript'),
+    email : document.getElementById('data-Email'),
+    social : document.getElementById('data-Social'),
+    imagen1 : document.getElementById('data-Imagen1'),
+    imagen2 : document.getElementById('data-Imagen2'),
+    imagen3 : document.getElementById('data-Imagen3'),
+    imagen4 : document.getElementById('data-Imagen4'),
+    imagen5 : document.getElementById('data-Imagen5')
+};
+
+const apiUrl = 'https://localhost:5001/hipermedia/fotos';
+
+const getnameData = async id =>{
+    const response = await fetch(`${apiUrl}/${id}`);
+    console.log(response);
+    const nameData = await response.json();
+    console.log(nameData);
+    const { names, lastName, age, tittle, descript, email, social, imagen1, imagen2, imagen3,
+        imagen4, imagen5 } = nameData;
+    photoData.names.innerText = `${names} ${lastName}`;
+    photoData.age.innerText = `Edad: ${age}`;
+    photoData.tittle.innerText = `Tema: ${tittle}`;
+    photoData.descript.innerText = `Descripción: ${descript}`;
+    photoData.email.innerText = `Contacto: ${email}`;
+    photoData.social.innerText = `${social}`;
+    photoData.imagen1.src = imagen1;
+    photoData.imagen2.src = imagen2;
+    photoData.imagen3.src = imagen3;
+    photoData.imagen4.src = imagen4;
+    photoData.imagen5.src = imagen5;
+}
+
+const url = new URL(window.location.href);
+
+const id = url.searchParams.get('id');
+
+getnameData(id);
